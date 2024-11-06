16c801fe34d5439f3aa724bdf3c9e45625cc6e6f
Theodorbjorkner <theodor.bjorkner@gmail.com>
2024-11-06 11:35:13 +0100

    added static code analysis code 4 testing

--- src/helloWorld.java
+++ src/helloWorld.java
@@ -1,5 +1,7 @@
 public class helloWorld {
     public static void main ( String [] args){
         System.out.println("Hello, world");
+        String path = System.getProperty("user.dir");
+        System.out.println("working dir:" + path);
     }
 }
